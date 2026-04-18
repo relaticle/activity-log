0e4fcd36cf5121c1835e36f69f58997728bd67c9
AsmitNepali <mail2asmitnepali@gmail.com>
2026-04-18 23:31:45 +0545

    Activity-log and Timeline Plugin Initiation.

---
+++ src/ActivityLogServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Relaticle\ActivityLog;
+
+use Illuminate\Contracts\Foundation\Application;
+use Livewire\Livewire;
+use Relaticle\ActivityLog\Renderers\RendererRegistry;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+final class ActivityLogServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('activity-log')
+            ->hasConfigFile('activity-log')
+            ->hasViews('activity-log')
+            ->hasTranslations();
+    }
+
+    public function packageRegistered(): void
+    {
+        $this->app->singleton(RendererRegistry::class, fn (Application $app): RendererRegistry => new RendererRegistry($app));
+        $this->app->singleton(Timeline\TimelineCache::class);
+    }
+
+    public function packageBooted(): void
+    {
+        Livewire::component('timeline-livewire', Filament\Livewire\TimelineLivewire::class);
+        Livewire::component('activity-log-livewire', Filament\Livewire\ActivityLogLivewire::class);
+    }
+}

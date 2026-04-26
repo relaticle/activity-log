7ef299fdb78686625cfa758c2a856307c8e93283
Manuk <manuk.minasyan1@gmail.com>
2026-04-26 17:56:20 +0400

    docs: add Activity Log logo + favicon (timeline mark, gradient palette)

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -42,6 +42,11 @@ export default defineNuxtConfig({
             link: [
                 {
                     rel: 'icon',
+                    type: 'image/svg+xml',
+                    href: baseURL + 'favicon.svg',
+                },
+                {
+                    rel: 'icon',
                     type: 'image/x-icon',
                     href: baseURL + 'favicon.ico',
                 },
